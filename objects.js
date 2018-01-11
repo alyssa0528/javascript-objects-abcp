2eac961cb46f7f4654fe9fab3d43f17cd46e1414
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-11 10:18:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = {
-  Slowdive: 'Alison',
-  'My Bloody Valentine': 'Sometimes'
+  "Slowdive": "Alison",
+  "My Bloody Valentine": "Sometimes"
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({playlist}, {'Phil Ochs': 'Here\'s to the State of Mississippi'})
+  Object.assign(playlist, {'Phil Ochs': "Here's to the State of Mississippi"})
   return playlist;
 }
 
