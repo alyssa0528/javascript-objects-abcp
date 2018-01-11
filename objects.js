2d5dc3d59664c975cacfe8e8a8b5b48ae209caf9
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-11 10:05:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {BSB: 'I Want It That Way'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign(playlist, {artistName: 'songTitle'})
+  Object.assign({playlist}, {artistName: 'songTitle'})
   return playlist;
 }
 
