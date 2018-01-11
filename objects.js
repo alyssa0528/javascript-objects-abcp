5a1c3d08f8bc9bef186446061b45485f0f3bed7b
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-11 10:01:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = {artist: song};
+var playlist = {artistName: songTitle};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  
+  return playlist;
+}
